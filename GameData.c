40b42429663be5a273f830b2c18da6a8ae190a4d
ggVzea <xenonruxy@gmail.com>
2025-12-25 10:40:28 +0700

    Add files via upload

---
+++ GameData.c
@@ -0,0 +1,18 @@
+
+local self = {}
+
+local World1, World2, World3 = nil, nil, nil
+local place = game.PlaceId
+
+function self.world()
+    if place == 2753915549 or place == 85211729168715 then
+        World1 = true
+    elseif place == 4442272183 or place == 79091703265657 then
+        World2 = true
+    elseif place == 7449423635 or place == 100117331123089 then
+        World3 = true
+    end
+    return World1, World2, World3
+end
+
+return self
